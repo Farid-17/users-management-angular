badfe5b82ad9a8a9e4e65968ef36a37aedb3e289
Farid-17 <hammani.farid17@gmail.com>
2023-06-11 01:39:26 +0100

    Fix problems in form validation

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -11,6 +11,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AddUserComponent implements OnInit, OnDestroy {
   public form!: FormGroup;
+  public isEdit: boolean = false;
 
   constructor(private http: HttpClient, private router: Router, private formBuilder: FormBuilder) {}
 
@@ -19,6 +20,7 @@ export class AddUserComponent implements OnInit, OnDestroy {
 
     if (linkWords.length > 3 && !["", null, undefined].includes(linkWords[linkWords.length - 1])) {
       global.data.id = parseInt(linkWords[linkWords.length - 1]);
+      this.isEdit = true;
     }
 
     this.formValidation();
@@ -29,10 +31,8 @@ export class AddUserComponent implements OnInit, OnDestroy {
   }
 
   public submitForm = () => {
-    // console.log(this.form.controls["first_name"]);
-
     if (!this.form.valid) {
-      global.showAlert("warning", "Please enter all formation correct!");
+      global.showAlert("warning", "Please enter all formations correct!");
       return;
     }
 
@@ -40,11 +40,13 @@ export class AddUserComponent implements OnInit, OnDestroy {
   }
 
   public formValidation = () => {
+    let numberPattern = "^(\\+212|0)(5|6|7)[0-9]{8}$"
+
     this.form = this.formBuilder.group({
       first_name: ["", Validators.required],
       last_name: ["", Validators.required],
       email: ["", [Validators.required, Validators.email]],
-      phone: ["", Validators.required],
+      phone: ["", [Validators.required, Validators.pattern(numberPattern)]],
       job: ["", Validators.required],
     });
   }
